4201d47b2f3cdc432da9f27a2a91879e898b8272
JasonZelmanovich <Jzelmano@mail.yu.edu>
2024-07-15 16:33:27 -0400

    init

---
+++ Backend/FinanceTrackerApplication/src/test/java/com/JasonRoth/financeTracker/FinanceTrackerApplication/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JasonRoth.financeTracker.FinanceTrackerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
