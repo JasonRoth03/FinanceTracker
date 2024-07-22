aeecbeac7e5391632e5671b437793ec37b61a930
JasonZelmanovich <Jzelmano@mail.yu.edu>
2024-07-21 22:43:06 -0400

    Allow Cors for frontend to access backend

--- Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/controllers/AuthController.java
+++ Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/controllers/AuthController.java
@@ -14,10 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -34,10 +31,11 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest){
         try{
-            System.out.println("Attempting to authenticate user: " + authRequest.getUsername());
+            System.out.println("Attempting to authenticate user: " + authRequest.getUsername() + authRequest.getPassword());
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
             System.out.println("Authentication successful for user: " + authRequest.getUsername());
             UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
@@ -52,6 +50,7 @@ public class AuthController {
 
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody AuthRequest authRequest){
         //Register the user
