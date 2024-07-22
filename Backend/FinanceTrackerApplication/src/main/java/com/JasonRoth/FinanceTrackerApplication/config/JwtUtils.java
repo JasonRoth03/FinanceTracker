234b691fefe44fefaf4f81adcc6300232176f127
JasonZelmanovich <Jzelmano@mail.yu.edu>
2024-07-22 15:49:16 -0400

    Simplified key

--- Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/JwtUtils.java
+++ Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/JwtUtils.java
@@ -2,39 +2,38 @@ package com.JasonRoth.FinanceTrackerApplication.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SecureDigestAlgorithm;
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtils {
-    private static final byte[] bytes = Decoders.BASE64.decode(JwtConstants.SECRET);
-    private static final Key key = Keys.hmacShaKeyFor(bytes);
-    private static final SecretKey sKey = Keys.hmacShaKeyFor(bytes);
 
     public static String generateToken(String username) {
             return Jwts.builder()
                     .subject(username)
                     .expiration(new Date(System.currentTimeMillis() + JwtConstants.EXPIRATION_TIME))
-                    .signWith(key)
+                    .signWith(key())
                     .compact();
+    }
+
+    private static Key key(){
+        System.out.println(JwtConstants.SECRET);
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(JwtConstants.SECRET));
     }
 
     public static Claims extractClaims(String token) {
         return Jwts.parser()
-                .verifyWith(sKey)
+                .verifyWith((SecretKey) key())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
-
     }
 
     public static boolean validateToken(String token) {
         try{
-            Jwts.parser().verifyWith(sKey).build().parseSignedClaims(token);
+            Jwts.parser().verifyWith((SecretKey) key()).build().parse(token);
             return true;
         }catch (Exception e){
             return false;
