695910113b559f9dfc7582c7e3b570260042cac2
JasonZelmanovich <Jzelmano@mail.yu.edu>
2024-07-24 13:08:28 -0400

    css for login & register

--- Frontend/financetrackerreact/src/components/Login.jsx
+++ Frontend/financetrackerreact/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import './modules/login.css'
 
 function Login(){
     const [username, setUsername] = useState("");
@@ -23,14 +24,16 @@ function Login(){
         navigate("/register");
     }
     return(
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required/>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
-                <button type="submit">Login</button>
-            </form>
-            <button onClick={handleRegister} type="button">Register</button>
+        <div className="app-container">
+            <div className="login-container">
+                <h2>Login</h2>
+                <form onSubmit={handleLogin}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+                    <button className="submit-button" type="submit">Login</button>
+                </form>
+                <button className="register-button" onClick={handleRegister} type="button" >Register Page</button>
+            </div>
         </div>
     );
 }
--- Frontend/financetrackerreact/src/components/Register.jsx
+++ Frontend/financetrackerreact/src/components/Register.jsx
@@ -1,31 +1,49 @@
 import {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import './modules/register.css';
 
 function Register() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
     const navigate = useNavigate();
 
     const handleRegister = async (e) => {
         e.preventDefault();
         try{
-            await axios.post("http://localhost:8080/api/register", {username, password});
+            await axios.post("http://localhost:8080/api/register", {firstName, lastName, username, password});
             navigate("/")
         }catch (error){
             console.log('register failed',error)
         }
     }
 
-    return(
-        <>
-            <h2>Register</h2>
-            <form onSubmit={handleRegister}>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required/>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
-                <button type="submit">Register</button>
-            </form>
-        </>
+    const handleLogin = () => {
+        navigate("/");
+    }
+
+    return (
+        <div className="app-container">
+            <div className="login-container">
+                <h2>Register</h2>
+                <form onSubmit={handleRegister}>
+                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}
+                           placeholder="First Name" required/>
+                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}
+                           placeholder="Last Name" required/>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}
+                           placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
+                           placeholder="Password" required/>
+                    <button className="submit-button" type="submit">Register</button>
+                </form>
+                <button className="register-button" onClick={handleLogin} type="button">Login Page</button>
+            </div>
+        </div>
     );
 }
+
 export default Register;
