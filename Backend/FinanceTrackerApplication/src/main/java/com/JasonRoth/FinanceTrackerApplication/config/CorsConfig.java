85562fa186bbf12dda0b618fd8acee335e2cf2cd
JasonZelmanovich <Jzelmano@mail.yu.edu>
2024-07-24 13:37:53 -0400

    Allow frontend connection

--- Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/CorsConfig.java
+++ Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                .allowedOrigins("https://jrfinancetracker.netlify.app/") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
