0e4a720cacc967977aeca99291462fb87fff01fb
JasonRoth <Jzelmano@mail.yu.edu>
2025-09-25 12:52:44 -0400

    add allowed origin

--- Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/CorsConfig.java
+++ Backend/FinanceTrackerApplication/src/main/java/com/JasonRoth/FinanceTrackerApplication/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://jrfinancetracker.netlify.app/") // Replace with your frontend URL
+                .allowedOrigins("https://jrfinancetracker.netlify.app/", "https://jason-roth.com/") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
